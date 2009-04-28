aabe2c69c73ec45da1f0fd435d0ebad6bcd27853
Jim Jeffers <jim@sumocreations.com>
2009-04-28 01:49:03 -0700

    Added sorting via class level and through proxy association.

--- lib/acts_as_debated.rb
+++ lib/acts_as_debated.rb
@@ -9,10 +9,40 @@ module ActiveRecord
       module ClassMethods
         def acts_as_debated(options = {})
           has_many :debateables, :as => :debated, :dependent => :destroy
+          extend ActiveRecord::Acts::Debated::SingletonMethods
           include ActiveRecord::Acts::Debated::InstanceMethods
         end
       end
-
+      
+      module OrderByScore
+        def order_by_score(sql={})
+          proxy_reflection.klass.order_by_score(sql.merge({:owner => proxy_owner}))
+        end
+      end
+        
+      module SingletonMethods
+        def order_by_score(sql={})
+          find_by_sql(" SELECT 
+                          target_objects.*,
+                          debateables.debated_id,
+                          debateables.debated_type, 
+                          sum(debateables.score) AS debated_score 
+                        FROM 
+                          #{table_name} target_objects, 
+                          #{Debateable.table_name} debateables 
+                        WHERE
+                          debateables.debated_id = target_objects.id AND
+                          debateables.debated_type = '#{name}'
+                          #{'AND target_objects.'+sql[:owner].class.name+'_id = '+sql[:owner].id.to_s if sql[:owner]}
+                        GROUP BY
+                          debateables.debated_id
+                        ORDER BY 
+                          debated_score #{sql[:order] || 'DESC'}
+                        #{'LIMIT '+sql[:limit].to_s if sql[:limit]} 
+                        #{'OFFSET '+sql[:offset].to_s if sql[:offset]}")
+        end
+      end
+      
       module InstanceMethods
         # Rates the object by a given score. A user object can be passed to the method.
         def debate_it( thumbs_up, user )
